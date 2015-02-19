2e9e7dc3ec15e602a23e52f029780b0759647cf5
Denise McCoy <Denise.L.McCoy@gmail.com>
2015-02-19 14:47:24 -0700

    upgraded ember

--- Brocfile.js
+++ Brocfile.js
@@ -1,11 +1,8 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
-var app = new EmberApp({
-  dotEnv: {
-    clientAllowedKeys: ['ADAPTER_URL']
-  }
-});
+
+var app = new EmberApp();
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
