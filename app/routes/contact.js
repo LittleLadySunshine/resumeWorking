e3c33d59ad823b018d210af7136111d026796505
Denise McCoy <Denise.L.McCoy@gmail.com>
2015-04-08 14:14:21 -0600

    no more facebook

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   didInsertElement: function() {
       Ember.run.scheduleOnce('afterRender', this, function() {
-          this.rerender();
+          this.refresh();
       });
   }
 });
